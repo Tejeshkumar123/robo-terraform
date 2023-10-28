674c44f9e1f32c19544c36e79375614db97e4d30
bindu <sindhudesalanka@gmail.com>
2023-10-28 10:39:57 +0530

    a

--- variables/var.tf
+++ variables/var.tf
@@ -1,13 +1,30 @@
+#####plane###
 variable "plane" {
   default = "mouni"
 }
 output "plane_output" {
   value = var.plane
 }
-
+####list###
 variable "list" {
   default = ["aws","devops","cloud"]
 }
 output "list_output" {
   value = var.list[2]
+}
+####map####
+variable "map" {
+  default = {
+    devops = {
+      time = "10am"
+      duration ="9r0 days"
+    }
+    aws = {
+      time = "11 am"
+      duration ="30 days"
+    }
+  }
+}
+output "map_outputs" {
+  value = var.map
 }
