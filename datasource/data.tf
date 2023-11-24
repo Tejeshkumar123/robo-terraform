1e7f3a3c7490af49079992e1bd586117d2867c8b
bindu <sindhudesalanka@gmail.com>
2023-11-24 12:50:20 +0530

    a

--- datasource/data.tf
+++ datasource/data.tf
@@ -1,12 +1,13 @@
 data "aws_ec2_spot_price" "example" {
   instance_type     = "t2.micro"
   availability_zone = "us-east-1a"
+
+  filter {
+    name   = "product-description"
+    values = ["Linux/UNIX"]
+  }
 }
 
-filter {
-  name   = "product-description"
-  values = ["Linux/UNIX"]
-}
 output "t2_micro_spot_price" {
   value = data.aws_ec2_spot_price.example.spot_price
 }
