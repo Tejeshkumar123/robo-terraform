dc4b769d97e1f35bd4b569d4b89cbcfa3107b17e
bindu <sindhudesalanka@gmail.com>
2023-11-24 12:48:23 +0530

    a

--- datasource/data.tf
+++ datasource/data.tf
@@ -8,7 +8,7 @@ filter {
   values = ["Linux/UNIX"]
 }
 output "t2_micro_spot_price" {
-  value = data.aws_ec2_spot_price.example
+  value = data.aws_ec2_spot_price.example.spot_price
 }
 data "aws_security_group" "selected" {
   id = "sg-0a1bb0374a177c7cf"
