be531444b5e9b746180c35e0893ae75437d6a536
bindu <sindhudesalanka@gmail.com>
2023-11-30 20:26:50 +0530

    a

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -37,7 +37,7 @@ data "aws_ami" "example" {
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
-#################security group terraform####################
+#################security group terraform#####################
 resource "aws_security_group" "sg" {
   name        = var.name
   description = "Allow TLS inbound traffic"
