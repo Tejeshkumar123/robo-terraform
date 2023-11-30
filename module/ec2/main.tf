6d1fe3e845470ce42ddb131a69131ded4eb66a34
bindu <sindhudesalanka@gmail.com>
2023-11-30 20:19:01 +0530

    a

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -1,3 +1,4 @@
+########### create ec2 instance using terraform ####
 resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
   instance_type = "t2.micro"
@@ -7,14 +8,36 @@ resource "aws_instance" "web" {
     Name = var.name
   }
 }
-
+############create route53 using terraform####################
+resource "aws_route53_record" "www" {
+  zone_id = "Z09755513LWICQ8RRTK8W"
+  name    = "${var.name}-dev"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.web.private_ip]
+}
+############create provisioners in terraform##############
+resource "null_resource" "ansible" {
+  depends_on = [aws_instance.web,aws_route53_record.www]
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.web.public_ip
+    }
+    inline = [
+      "sudo set-hostname ${var.name}"
+    ]
+  }
+}
+########### ami id datasource terraform ##############
 data "aws_ami" "example" {
-  executable_users = ["self"]
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["973714476881"]
 }
-
+#################security group terraform####################
 resource "aws_security_group" "sg" {
   name        = var.name
   description = "Allow TLS inbound traffic"
